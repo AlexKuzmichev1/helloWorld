d0839639e7c403d137f36d59844345d00f16d30c
Alex Kuzmichev <alexxxanderkuzmichev@gmail.com>
2022-06-16 13:04:26 +0300

    Initial Commit

---
+++ helloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloWorld
+//
+//  Created by 1 on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
